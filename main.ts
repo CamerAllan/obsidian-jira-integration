d11ad90791923426b0804eb8acb4e26a22a1d494
cameroa <cameroa@dunnhumby.com>
2021-10-29 10:25:18 +0100

    Functional jira integration

--- main.ts
+++ main.ts
@@ -1,79 +1,104 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting, TAbstractFile, TFile, Vault } from 'obsidian';
+import JiraApi from 'jira-client';
+import * as Eta from 'eta';
 
+interface Issue {
+	key: string;
+	self: string;
+	fields: {
+		summary: string;
+		description: string;
+	}
+}
 interface MyPluginSettings {
-	mySetting: string;
+	token: string;
+	host: string;
+	templateFilePath: string;
 }
 
 const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
+	token: '',
+	host: '',
+	templateFilePath: ''
 }
 
 export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
+	jira: JiraApi;
 
 	async onload() {
 		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		let ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+		const jira = new JiraApi({
+			protocol: 'https',
+			host: this.settings.host,
+			bearer: this.settings.token,
+			apiVersion: '2',
+			strictSSL: false
 		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+
+		const hydrateTemplate = async (templateFile: TFile, issue: Issue): Promise<string | void> => {
+			const host = issue.self.substring(0, issue.self.indexOf("/rest/"))
+			const uiLink = host + "/browse/" + issue.key
+			const template = await templateFile.vault.read(templateFile)
+
+			const t = {
+				link: uiLink,
+			}
+
+			// Allow all issue properties to be templated
+			Object.assign(t, issue)
+
+			return Eta.render(template, t) as string | void
+		}
+
+		const getIssueById = async (jira: JiraApi, issueId: string): Promise<Issue> => {
+			var issue = null
+			try {
+				issue = await jira.findIssue(issueId)
+				console.log(`Status: ${issue.fields.status.name}`);
+			} catch (err) {
+				console.error(err)
+			}
+			return issue as Issue
+		}
+
+		const addIssueContentsToFile = async (file: TFile, templateFilePath: string, issue: Issue) => {
+			const template = file.vault.getAbstractFileByPath(templateFilePath) as TFile
+			const hydratedTemplate = await hydrateTemplate(template, issue)
+			if (hydrateTemplate) {
+				file.vault.modify(file, hydratedTemplate as string)
+			} else {
+				console.error("Sadness")
+			}
+		}
+
+		this.addCommand({
+			id: 'jira-hydrate-ticket',
+			name: 'Add Jira issue info',
+			callback: async () => {
+				const activeFile = this.app.workspace.getActiveFile()
+				// Trim '.md' from filename
+				const activeFilename = activeFile.name.substr(0, activeFile.name.length - 3)
+				const jiraIssue = await getIssueById(jira, activeFilename)
+				if (jiraIssue != null) {
+					console.debug(`Filename ${activeFilename} matches Jira issue number!`)
+					addIssueContentsToFile(activeFile as TFile, this.settings.templateFilePath, jiraIssue)
+				}
+			}
+		});
+
+		// Hook for when a file is renamed
+		this.app.vault.on("rename", async function(file, _oldName) {
+			// Trim .md
+		})
 
 		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		let statusBarItemEl = this.addStatusBarItem();
 		statusBarItemEl.setText('Status Bar Text');
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
-
 		// This adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -87,6 +112,7 @@ export default class MyPlugin extends Plugin {
 	async saveSettings() {
 		await this.saveData(this.settings);
 	}
+
 }
 
 class SampleModal extends Modal {
@@ -118,17 +144,34 @@ class SampleSettingTab extends PluginSettingTab {
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', {text: 'Settings for Jira.'});
 
 		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
+			.setName('Host')
+			.setDesc('Jira hostname')
 			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
+				.setPlaceholder('jira.example.com')
+				.setValue(this.plugin.settings.host)
 				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
+					this.plugin.settings.host = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Jira PAT')
+			.setDesc('Personal Access Token. Generate by going to Profile -> Personal Access Tokens.')
+			.addText(text => text
+				.setValue(this.plugin.settings.token)
+				.onChange(async (value) => {
+					this.plugin.settings.token = value;
+					await this.plugin.saveSettings();
+				}));
+		new Setting(containerEl)
+			.setName('Issue Template')
+			.setDesc('Path to template file which will be hydrated with Jira issue contents')
+			.addText(text => text
+				.setValue(this.plugin.settings.templateFilePath)
+				.onChange(async (value) => {
+					this.plugin.settings.templateFilePath = value;
 					await this.plugin.saveSettings();
 				}));
 	}
